e1f05a458f350fe5d92df1e9f613858465882db3
Michael H Reed <mhr2126@gmail.com>
2011-10-24 20:20:13 -0400

    finished changing classifier_1. need to add tester for the two of them

--- classifier_1.m
+++ classifier_1.m
@@ -1,9 +1,9 @@
 function classifier_1(trainpath, testpath, outtrainpath, outtestpath)
 
 % read file and set up variables
-	fTrainIn = fopen(trainpath, 'r');
-	C = textscan(fTrainIn, '%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
-	fclose(fTrainIn);
+	trainFile = fopen(trainpath, 'r');
+	C = textscan(trainFile, '%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+	fclose(trainFile);
 
 	sample = cell2mat(C(2:end));
 	sampleLabel = C{1};
@@ -11,33 +11,37 @@ function classifier_1(trainpath, testpath, outtrainpath, outtestpath)
 
 	labelSet = unique(sampleLabel);
 
-	mu = repmat(mean(sample), size(sample, 1), 1);
-	sigma = repmat(sqrt(var(sample)), size(sample, 1), 1);
-	sample = (sample - mu)./sigma;
-
-	good = select_features(sample, perClass, 11)
+	good = select_features(sample, perClass, 2)
 	sample = sample(:, good);
 
-% whitening transform
-	[V, D] = eig(cov(sample));
-	Aw = V*D^(-0.5);
-	mu = repmat(mean(sample), size(sample, 1), 1);
-	sample = (Aw*sample')';
+% normalize using mean and sd
+	sampleMu = repmat(mean(sample), size(sample, 1), 1);
+	sampleSigma = repmat(sqrt(var(sample)), size(sample, 1), 1);
+	sample = (sample - sampleMu)./sampleSigma;
 
-	train = zeros(size(sample, 1) - size(labelSet, 1), size(sample, 2));
-	validate = zeros(size(labelSet, 1), size(sample, 2));
-	correct = zeros(perClass-1, 1);
+	correct = zeros(perClass, 1);
+	bestSuccess = 0;
+	classifierLabel = {};
+	classifier = [];
+	testLabel = {};
+	test = [];
+	step = round(0.7*perClass);
 
-	for offset = 1:perClass-1
+	for offset = 0:perClass-1
 
+		trainCount = zeros(1, size(labelSet, 1));
+		testCount = zeros(1, size(labelSet, 1));
+		validate = [];
+		train = [];
 		validateLabel = {};
 		trainLabel = {};
 
 % split samples for cross validation
 		i = 1;
 		j = 1;
+
 		for k = 1:size(sample, 1)
-			if mod(k - offset, perClass)
+			if mod(k - offset, perClass) < step
 				trainLabel{end+1} = sampleLabel{k};
 				train(i, :) = sample(k, :);
 				i = i + 1;
@@ -51,14 +55,14 @@ function classifier_1(trainpath, testpath, outtrainpath, outtestpath)
 		sigma = zeros(size(labelSet, 1)*size(train, 2), size(train, 2));
 		mu = zeros(size(labelSet, 1), size(train, 2));
 		for c = 1:size(labelSet, 1)
-			X = train(1+(c-1)*(perClass-1):c*(perClass-2), :);
+			X = train(1+(c-1)*(step):c*(step-1), :);
 			mu(c, :) = mean(X);
 			k = 1 + (c-1)*size(train, 2);
 			sigma(k:k+size(train, 2)-1, :) = cov(X);
 		end
 
-		b = min(min(sigma));
-		if b < 0 sigma = 1 + sigma - b; end
+%		b = min(min(sigma));
+%		if b < 0 sigma = 1 + sigma - b; end
 
 		best = zeros(size(validate, 1), 2);
 		best(:) = -Inf;
@@ -77,23 +81,61 @@ function classifier_1(trainpath, testpath, outtrainpath, outtestpath)
 		for i = 1:size(validate, 1)
 			class = labelSet{best(i, 2)};
 			if strcmp(class, validateLabel{i})
-				correct(offset) = correct(offset) + 1;
+				correct(offset+1) = correct(offset+1) + 1;
 			end
 			validateLabel{i} = class;
 		end
+		correct(offset+1) = correct(offset+1)/size(validate, 1);
+		if correct(offset+1) > bestSuccess
+			bestSuccess = correct(offset+1);
+			classifierLabel = trainLabel;
+			classifier = train;
+			testLabel = validateLabel;
+			test = validate;
+		end
+
+		assert(sum(trainCount) == size(labelSet, 1)*trainCount(1));
+		assert(sum(testCount) == size(labelSet, 1)*testCount(1));
 	end
 
-	correct/size(labelSet, 1)
-	[s, i] = sort(correct, 'descend');
-	i(1)
+	bestSuccess
+	correct
 
-	fTrainOut = fopen(outtrainpath, 'w');
-	for ix = 1:size(validate, 1)
-		fprintf(fTrainOut, '%s', validateLabel{ix});
-		fprintf(fTrainOut, ' %f', validate(ix,:));
-		fprintf(fTrainOut, '\n');
+	trainFile = fopen(outtrainpath, 'w');
+	for i = 1:size(test, 1)
+		fprintf(trainFile, '%s %f ', testLabel{i}, test(i,:));
+		fprintf(trainFile, '\n');
 	end
-	fclose(fTrainOut);
+	fclose(trainFile);
+
+% read and classify testing data
+	testFile = fopen(testpath, 'r');
+	C = textscan(testFile, '%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+	fclose(testFile);
+
+	sample = cell2mat(C(2:end));
+	sampleLabel = C{1};
+
+	sample = sample(:, good);
+	sample = (sample - sampleMu)./sampleSigma;
+
+	testFile = fopen(outtestpath, 'w');
+	for i = 1:size(sample, 1)
+		c = sampleLabel{i};
+		x = sample(i);
+		best = Inf;
+
+		for j = 1:size(classifier, 1)
+			d = norm(x - classifier(j, :));
+			if d < best
+				c = classifierLabel{j};
+				best = d;
+			end
+		end
+		fprintf(testFile, '%s %f ', c, sample(i, :));
+		fprintf(testFile, '\n');
+	end
+	fclose(testFile);
 
 function [p] = log_mvnpdf(x, m, s)
 
--- classifier_2.m
+++ classifier_2.m
@@ -1,9 +1,9 @@
 function classifier_2(trainpath, testpath, outtrainpath, outtestpath)
 
 % read file and set up variables
-	fTrainIn = fopen(trainpath, 'r');
-	C = textscan(fTrainIn, '%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
-	fclose(fTrainIn);
+	trainFile = fopen(trainpath, 'r');
+	C = textscan(trainFile, '%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+	fclose(trainFile);
 
 	sample = cell2mat(C(2:end));
 	sampleLabel = C{1};
@@ -11,17 +11,28 @@ function classifier_2(trainpath, testpath, outtrainpath, outtestpath)
 
 	labelSet = unique(sampleLabel);
 
+% select the best features
+	good = select_features(sample, perClass, 11);
+	sample = sample(:, good);
+
 % normalize using mean and sd
-	mu = repmat(mean(sample), size(sample, 1), 1);
-	sigma = repmat(sqrt(var(sample)), size(sample, 1), 1);
-	sample = (sample - mu)./sigma;
+	sampleMu = repmat(mean(sample), size(sample, 1), 1);
+	sampleSigma = repmat(sqrt(var(sample)), size(sample, 1), 1);
+	sample = (sample - sampleMu)./sampleSigma;
 
-	train = zeros(size(sample, 1) - size(labelSet, 1), size(sample, 2));
-	validate = zeros(size(labelSet, 1), size(sample, 2));
-	correct = zeros(perClass-1, 1);
+	correct = zeros(perClass, 1);
+	bestSuccess = 0;
+	classifierLabel = {};
+	classifier = [];
+	testLabel = {};
+	test = [];
 
-	for offset = 1:perClass-1
+	for offset = 0:perClass-1
 
+		trainCount = zeros(1, size(labelSet, 1));
+		testCount = zeros(1, size(labelSet, 1));
+		validate = [];
+		train = [];
 		validateLabel = {};
 		trainLabel = {};
 
@@ -29,18 +40,21 @@ function classifier_2(trainpath, testpath, outtrainpath, outtestpath)
 		i = 1;
 		j = 1;
 		for k = 1:size(sample, 1)
-			if mod(k - offset, perClass)
+			r = find(ismember(labelSet, sampleLabel{k}) == 1);
+			if mod(k - offset, perClass) < round(0.7*perClass)
 				trainLabel{end+1} = sampleLabel{k};
+				trainCount(r) = trainCount(r) + 1;
 				train(i, :) = sample(k, :);
 				i = i + 1;
 			else
 				validateLabel{end+1} = sampleLabel{k};
+				testCount(r) = testCount(r) + 1;
 				validate(j, :) = sample(k, :);
 				j = j + 1;
 			end
 		end
 
-		k = 1;
+%classify the test portion of the training data using the nearest neighbor rule
 		for i = 1:size(validate, 1)
 			x = validate(i, :);
 			best = [Inf, 1];
@@ -54,20 +68,58 @@ function classifier_2(trainpath, testpath, outtrainpath, outtestpath)
 			end
 			class = labelSet{best( 2)};
 			if strcmp(class, validateLabel{i})
-				correct(offset) = correct(offset) + 1;
+				correct(offset+1) = correct(offset+1) + 1;
 			end
 			validateLabel{i} = class;
 		end
+		correct(offset+1) = correct(offset+1)/size(validate, 1);
+		if correct(offset+1) > bestSuccess
+			bestSuccess = correct(offset+1);
+			classifierLabel = trainLabel;
+			classifier = train;
+			testLabel = validateLabel;
+			test = validate;
+		end
+
+		assert(sum(trainCount) == size(labelSet, 1)*trainCount(1));
+		assert(sum(testCount) == size(labelSet, 1)*testCount(1));
 	end
 
-	correct/size(labelSet, 1)
-	[s, i] = sort(correct, 'descend');
-	i(1)
+	bestSuccess
+	correct
 
-	fTrainOut = fopen(outtrainpath, 'w');
-	for ix = 1:size(validate, 1)
-		fprintf(fTrainOut, '%s', validateLabel{ix});
-		fprintf(fTrainOut, ' %f', validate(ix,:));
-		fprintf(fTrainOut, '\n');
+	trainFile = fopen(outtrainpath, 'w');
+	for i = 1:size(test, 1)
+		fprintf(trainFile, '%s %f ', testLabel{i}, test(i,:));
+		fprintf(trainFile, '\n');
 	end
-	fclose(fTrainOut);
+	fclose(trainFile);
+
+% read and classify testing data
+	testFile = fopen(testpath, 'r');
+	C = textscan(testFile, '%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f');
+	fclose(testFile);
+
+	sample = cell2mat(C(2:end));
+	sampleLabel = C{1};
+
+	sample = sample(:, good);
+	sample = (sample - sampleMu)./sampleSigma;
+
+	testFile = fopen(outtestpath, 'w');
+	for i = 1:size(sample, 1)
+		c = sampleLabel{i};
+		x = sample(i);
+		best = Inf;
+
+		for j = 1:size(classifier, 1)
+			d = norm(x - classifier(j, :));
+			if d < best
+				c = classifierLabel{j};
+				best = d;
+			end
+		end
+		fprintf(testFile, '%s %f ', c, sample(i, :));
+		fprintf(testFile, '\n');
+	end
+	fclose(testFile);
